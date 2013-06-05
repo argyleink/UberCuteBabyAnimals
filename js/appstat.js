390dca2a50400f058ef0b3d2b68a12a81dfaeb4a
Adam Argyle <atom@argyleink.com>
2013-06-05 00:20:17 -0700

    adding missing file

---
+++ js/appstat.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+
+    var element
+      , showing = false;
+
+    function show() {
+        appstatus.classList.remove('out');
+    }
+
+    function hide() {
+        appstatus.classList.add('out');
+    }
+
+    WinJS.Namespace.define("Appstat", {
+        show: show,
+        hide: hide
+    });
+
+})();
